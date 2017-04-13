ab7d6ae29a457e492e4b36997236d7d7421e9275
Ben Bledsoe <bledsoedesigns@gmail.com>
2017-04-12 20:21:25 -0500

    Inital commit, setting project structure

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// kickoff react app
+ReactDOM.render(
+  // pass jsx
+  <h1>Boilerplateapp</h1>,
+  // pass dom node
+  document.getElementById('app')
+);
